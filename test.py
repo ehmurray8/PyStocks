0ab4fbc9ed41fabc8a84c6007e4c67e99bc935ac
Emmet Murray <ehmurray8@gmail.com>
2017-10-21 16:57:25 -0400

    Added stock reading logic to test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import http.client
+import pandas as pd
 
 conn = http.client.HTTPConnection("download.finance.yahoo.com")
 
@@ -7,9 +8,30 @@ headers = {
     'postman-token': "c6c3e450-15eb-4d8b-1095-e4abdcb6d51a"
     }
 
-conn.request("GET", "/d/quotes.csv?s=ZNWAA%20ZION%20ZIONW%20ZIONZ%20ZIOP%20ZIXI%20ZGNX%20ZSAN%20ZUMZ%20ZYNE%20ZNGA%0A&f=nspomkjm3m4va5b6a2", headers=headers)
 
-res = conn.getresponse()
-data = res.read()
+entries = pd.read_csv("stockinfo.csv", skip_blank_lines=True)
+count = 0
+url_exts = []
+url = "/d/quotes.csv?s="
+for e in entries['Symbol']:
+    url += "%20" + e
+    count += 1
+    if count == 200:
+        url += "&f=nspomkjm3m4va5b6a2"
+        url_exts.append(url)
+        url = "/d/quotes.csv?s="
+        count = 0
 
-print(data.decode("utf-8"))
+if count != 200:
+    url += "&f=nspomkjm3m4va5b6a2"
+    url_exts.append(url)
+
+for u in url_exts:
+    #conn.request("GET", "/d/quotes.csv?s=ZNWAA%20ZION%20ZIONW%20ZIONZ%20ZIOP%20ZIXI%20ZGNX%20ZSAN%20ZUMZ%20ZYNE%20ZNGA%0A&f=nspomkjm3m4va5b6a2", headers=headers)
+    conn.request("GET", u, headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+    print("\n\n")
